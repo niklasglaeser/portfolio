bb9bebf5beae9423745e87d2214e54ecde03c6cd
niklas-glaeser <123083964+niklas-glaeser@users.noreply.github.com>
2024-09-17 17:54:12 +0200

    adds third project in the display

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { BurgerMenuComponent } from "../burger-menu/burger-menu.component";
 import { FooterComponent } from "../footer/footer.component";
@@ -11,7 +11,31 @@ import { TranslateModule, TranslateService} from '@ngx-translate/core';
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
-export class LegalNoticeComponent {
+export class LegalNoticeComponent implements OnInit {
   constructor(private translate: TranslateService) {}
+
+  ngOnInit() {
+    this.addScrollAnimations();
+  }
+
+  addScrollAnimations() {
+    const elements = document.querySelectorAll('.scroll-animation');
+    
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    }, {
+      threshold: 0.2
+    });
+
+    elements.forEach(element => {
+      observer.observe(element);
+    });
+  }
 
 }
